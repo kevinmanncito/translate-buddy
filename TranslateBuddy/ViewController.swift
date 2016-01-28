a3d1065b3c9636e3520c36add38e553c366e3127
Kevin Mann <kevinrmann@gmail.com>
2016-01-27 17:06:21 -0700

    Initial Commit

---
+++ TranslateBuddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TranslateBuddy
+//
+//  Created by Kevin Mann on 1/27/16.
+//  Copyright © 2016 Freedom Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
